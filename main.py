cf5c9e55ee3a5e8e011d10cde75d539c0ec59cc0
Joostluijben <joost.luijben@student.hu.nl>
2018-01-22 13:03:55 +0100

    Made database available

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("hello")
+import mysql.connector
+cnx = mysql.connector.connect(user='root', password='idp',
+                              host='127.0.0.1',
+                              database='idp_project')
+cnx.close()
