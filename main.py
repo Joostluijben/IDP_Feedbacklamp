8405dca99d4336de3b43e1bc5af8e712087d836e
Joostluijben <joost.luijben@student.hu.nl>
2018-01-23 14:10:03 +0100

    First files commit

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from flask import Flask
+from flask import render_template
 import mysql.connector
-cnx = mysql.connector.connect(user='root', password='idp',
-                              host='127.0.0.1',
-                              database='idp_project')
-cnx.close()
+from flask import request
+
+
+app = Flask(__name__)
+@app.route('/', methods=['POST', 'GET'])
+def main():
+    cnx = mysql.connector.connect(user='root', password='idp',
+                                  host='127.0.0.1',
+                                  database='idp_project')
+    cnx.close()
+    return render_template('index.html')
+app.run('0.0.0.0', 8080)
