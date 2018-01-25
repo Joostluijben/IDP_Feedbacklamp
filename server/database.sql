3235dd972767b252095b2d04b45baebde9abfafb
Maxim <maxim.stephan@student.hu.nl>
2018-01-25 13:21:12 +0100

    Updated data

--- server/database.sql
+++ server/database.sql
@@ -9,12 +9,7 @@ Wachtwoord VARCHAR(50),
 Email VARCHAR(50),
 PRIMARY KEY(DocentID)
 );
-CREATE TABLE Klas  (
-KlasID INT NOT NULL AUTO_INCREMENT,
-DocentID INT NOT NULL,
-LampID INT NOT NULL,  
-PRIMARY KEY(KlasID)
-);
+
 
 CREATE TABLE Lamp  (
 LampID INT NOT NULL AUTO_INCREMENT,
@@ -24,19 +19,10 @@ PRIMARY KEY(LampID)
 
 CREATE TABLE Sensor  (
 SensorID INT NOT NULL AUTO_INCREMENT,
-Decibel INT NOT NULL,
+LampID INT NOT NULL ,
 Tijd DATETIME,
 PRIMARY KEY(SensorID)
 );
-
-
-ALTER TABLE Klas
-ADD CONSTRAINT Klas_Docent
-FOREIGN KEY (DocentID) REFERENCES Docent(DocentID);
-
-ALTER TABLE Klas
-ADD CONSTRAINT Klas_Lamp
-FOREIGN KEY (LampID) REFERENCES Lamp(LampID);
 
 ALTER TABLE Lamp
 ADD CONSTRAINT Lamp_Sensor
@@ -56,3 +42,17 @@ VALUES ('Eva_Dijk', 'Eva123', 'eva.dijk@mail.com');
 
 INSERT INTO Docent (Gebruikersnaam, Wachtwoord, Email)
 VALUES ('Sara_Bruin', 'Sara123', 'sara.bruin@mail.com');
+
+INSERT INTO Sensor (SensorID, LampID)
+VALUES ('1', '001');
+
+INSERT INTO Sensor (SensorID, LampID)
+VALUES ('2', '001');
+
+INSERT INTO Sensor (SensorID, LampID)
+VALUES ('3', '001');
+
+INSERT INTO lamp (LampID, SensorID)
+VALUES ('001', '1'); #2 en 3 moeten er ook nog bij#
+
+#decibel en Gem_Decibel en DocentID bij lamp en foreign key van docent_lamp moeten er nog bij#
