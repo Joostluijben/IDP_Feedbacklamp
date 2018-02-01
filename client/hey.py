b0f1c93dc790c4185a11ccaa5e3119d3e7b87fc9
Joostluijben <joost.luijben@student.hu.nl>
2018-02-01 12:24:58 +0100

    Added comments

--- client/hey.py
+++ client/hey.py
@@ -2,6 +2,7 @@ import pyaudio
 import audioop
 import numpy as np
 import time
+# initialise pyaudio get channels and rate automaticly, also set chunk size
 pa = pyaudio.PyAudio()
 CHUNK = 16
 RATE = int(pa.get_device_info_by_index(2).get('defaultSampleRate'))
@@ -13,8 +14,8 @@ stream = pa.open(format = pyaudio.paInt16,
     frames_per_buffer = 16,
     input_device_index=2)
 def readAudio():
-
     audioLst = []
+    # calculate the root mean square of every chunk and put it in a list. Return the average of this list
     for i in range(1000):
         data = stream.read(CHUNK, exception_on_overflow=False)
         rms = audioop.rms(data,2)
--- server/main.py
+++ server/main.py
@@ -3,17 +3,16 @@ import mysql.connector
 import requests
 import time
 import threading
-import requests
 from bs4 import BeautifulSoup
 import datetime
 import time
-from functools import wraps
 import random
 import hashlib
 import binascii
 import RPi.GPIO as GPIO
 
 app = Flask(__name__)
+# configure salt and set GPIO pins
 salt = b'\xf5\xbb;\xd8S\xeb\x0b\xf8\xe8\xef\x9d\xab\xa1\xce<\x8c\xfeIP\xfc'
 loudGPIO = 7
 averageGPIO = 11
@@ -24,14 +23,17 @@ GPIO.setup(loudGPIO, GPIO.OUT)
 GPIO.setup(averageGPIO, GPIO.OUT)
 GPIO.setup(silenceGPIO, GPIO.OUT)
 
+# set the secret key for flask sessions
 app.secret_key = 'some_secret'
 
 @app.route('/', methods=['POST', 'GET'])
 def main():
+    # return the login page
     error = None
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        # compare entered password against hashed pbkdf2 password in database
         hashedPassword = (binascii.hexlify(hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 100000))).decode()
         classRoom = request.form['classRoom']
         db = mysql.connector.connect(user='root', password='idp',
@@ -42,6 +44,7 @@ def main():
         for row in cursor:
             if username == row[0] and hashedPassword == row[1]:
                 cursor.execute("SELECT Klas FROM Lamp;")
+                # check if classroom exists else give user error message
                 for schoolClassDb in cursor:
                     if schoolClassDb[0] == classRoom:
                         return redirect(url_for('overview', username=username, classRoom=classRoom))
@@ -57,6 +60,7 @@ def main():
 
 @app.route('/overview/<username>/<classRoom>', methods=['POST', 'GET'])
 def overview(username, classRoom):
+    # show the overview, user can select sensivity for sensor
     if request.method == 'POST':
         maxValue = request.form['maxValue']
 
@@ -66,27 +70,32 @@ def overview(username, classRoom):
 measurements = []
 @app.route('/process/<username>/<classRoom>/<maxValue>', methods=['POST', 'GET'])
 def process(username,classRoom, maxValue):
+    # show the process with the dynamic table
     error = None
     if request.method == 'POST':
+        # on stop measurement turn LED's of and save sessiondata in database
         GPIO.output(loudGPIO, GPIO.LOW)
         GPIO.output(averageGPIO, GPIO.LOW)
         GPIO.output(silenceGPIO,GPIO.LOW)
+        # get the average measurement
         try:
             average = sum(measurements)/len(measurements)
         except ZeroDivisionError:
             return redirect(url_for('overview', username=username, classRoom=classRoom))
         starttime = session.get('starttime')
         endtime = datetime.datetime.now()
+        duration = endtime - starttime
         db = mysql.connector.connect(user='root', password='idp',
                                       host='127.0.0.1',
                                       database='idp_project')
         cursor = db.cursor()
         try:
+            # find themathcing  teacher and classroom in the database
             cursor.execute("SELECT DocentID FROM Docent WHERE Gebruikersnaam = %s;", (username,))
             docentID = cursor.fetchone()[0]
             cursor.execute("SELECT LampID FROM Lamp WHERE Klas = %s;", (classRoom,))
             lampID = cursor.fetchone()[0]
-            cursor.execute("""INSERT INTO Sessie(Gemiddelde, Starttijd, Eindtijd, DocentID, LampID) VALUES (%s, %s, %s, %s, %s);""", (average, starttime, endtime, docentID, lampID))
+            cursor.execute("""INSERT INTO Sessie(Gemiddelde, Starttijd, Eindtijd, Duur, DocentID, LampID) VALUES (%s, %s, %s, %s, %s, %s);""", (average, starttime, endtime, duration, docentID, lampID))
             cursor.close()
             db.commit()
             db.close()
@@ -97,33 +106,32 @@ def process(username,classRoom, maxValue):
     return render_template('process.html', maxValue=maxValue, error=error)
 @app.route('/measure_values/<maxValue>')
 def measure_values(maxValue):
-
+    # get the set values from the overview.html and put them in a list
     setValues = [int(value) for value in maxValue.split(',')]
-    print(setValues)
+    # turn LED's off on startup
     GPIO.output(loudGPIO, GPIO.LOW)
     GPIO.output(averageGPIO, GPIO.LOW)
     GPIO.output(silenceGPIO,GPIO.LOW)
     starttime = datetime.datetime.now()
     session['starttime'] = starttime
-
     def generate():
         while True:
             try:
-                page = requests.get('https://192.168.1.27:8080')
+                page = requests.get('http://192.168.1.1:8080')
                 soup = BeautifulSoup(page.content, 'lxml')
                 found = float(soup.find('div', class_='volumeValue').text.strip())
                 color = None
-                #found = random.randrange(1,11)
-                now = datetime.datetime.now().strftime('%H:%M:%S %Y-%m-%d')
+                # compare shown values against set values
                 if found > setValues[1]:
                     color = 'Rood'
                 elif found > setValues[0] and found < setValues[1]:
                     color = 'Geel'
                 else:
                     color = 'Groen'
+                # generate string with the values
                 yield '{}, {},{}\n'.format(now,found,color)
 
-                time.sleep(5)
+                time.sleep(0.5)
                 GPIO.output(loudGPIO, GPIO.LOW)
                 GPIO.output(averageGPIO, GPIO.LOW)
                 GPIO.output(silenceGPIO,GPIO.LOW)
@@ -134,11 +142,12 @@ def measure_values(maxValue):
                 else:
                     GPIO.output(silenceGPIO, GPIO.HIGH)
                 measurements.append(found)
+            # on error set LED's off
             except requests.exceptions.ConnectionError:
                 GPIO.output(loudGPIO, GPIO.LOW)
                 GPIO.output(averageGPIO, GPIO.LOW)
                 GPIO.output(silenceGPIO,GPIO.LOW)
-                raise
+                pass
             except:
                 GPIO.output(loudGPIO, GPIO.LOW)
                 GPIO.output(averageGPIO, GPIO.LOW)
@@ -146,5 +155,5 @@ def measure_values(maxValue):
                 raise
     return app.response_class(generate())
 
-
-app.run('0.0.0.0', 8080, debug=True, threaded=True, ssl_context=('cert.pem', 'key.pem'))
+# start the application. Threaded so multiple users can login and force https with self signed certificate
+app.run('0.0.0.0', 8080, threaded=True, ssl_context=('cert.pem', 'key.pem'))
